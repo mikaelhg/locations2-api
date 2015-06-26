b09c72a8db3f899e4e4c8e229278acfe7b658aee
Mikael Gueck <gumi@iki.fi>
2015-06-26 16:33:00 +0300

    Exclude integration tests from running by default.

--- src/test/java/io/mikael/loc2/IntegrationTests.java
+++ src/test/java/io/mikael/loc2/IntegrationTests.java
@@ -4,6 +4,7 @@ import com.jayway.restassured.RestAssured;
 import org.apache.http.HttpStatus;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,6 +26,7 @@ import static org.junit.Assert.assertEquals;
 @WebAppConfiguration
 @IntegrationTest({"server.port=0", "spring.cache.type=none"})
 @DirtiesContext
+@Category(IntegrationTests.class)
 public class IntegrationTests {
 
     @Value("${local.server.port}")
